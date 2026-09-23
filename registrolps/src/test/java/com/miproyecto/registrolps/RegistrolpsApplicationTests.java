b840abd4eb3559e9564f90b6c22985305d8cb274
nathSD <nath.santiago.dev@gmail.com>
2026-09-23 16:44:11 -0500

    Corregir dependencias de pruebas

--- registrolps/src/test/java/com/miproyecto/registrolps/RegistrolpsApplicationTests.java
+++ registrolps/src/test/java/com/miproyecto/registrolps/RegistrolpsApplicationTests.java
@@ -6,8 +6,8 @@ import com.miproyecto.registrolps.entity.enums.TipoUsuario;
 import com.miproyecto.registrolps.service.UsuarioService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
